2241fa8ebd403e376c65779c121b9b3f0a142c52
Scoobi-wisdoom <grevod@gmail.com>
2023-05-29 17:43:35 +0900

    add a function to save hack file

--- src/main/kotlin/Assembler.kt
+++ src/main/kotlin/Assembler.kt
@@ -14,12 +14,39 @@ fun main() {
         readAssembly.parseCommands(lines)
     }
 
+    val convertAssembly = ConvertAssembly()
+    val fileNameToConvertedCommands: Map<String, List<String>> = fileNameToCommands.mapValues { (_, commands) ->
+        commands.map { convertAssembly.convert(it) }
+    }
 
+    fileNameToConvertedCommands.entries.forEach {
+        createFiles(
+            exisingFilePath = it.key,
+            newExtension = HACK_EXTENSION,
+            content = it.value,
+        )
+    }
 }
 
 private fun getLines(fileName: String): List<String> {
     return File(fileName).useLines {
         it.toList()
+    }
+}
+
+private fun createFiles(
+    exisingFilePath: String,
+    newExtension: String,
+    content: List<String>,
+) {
+    val existingFileNameWithoutExtension = exisingFilePath.substringAfterLast("/").substringBefore(".")
+    val filePath = "${exisingFilePath.substringBeforeLast("/")}/$existingFileNameWithoutExtension$newExtension"
+
+    File(filePath).bufferedWriter().use { writer ->
+        content.forEach { line ->
+            writer.write(line)
+            writer.newLine()
+        }
     }
 }
 
