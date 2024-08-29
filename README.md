c0d39b5fca708a9aafc3849d0c93da9f406cad10
Scoobi-wisdoom <grevod@gmail.com>
2024-08-29 21:18:33 +0900

    impl code writer: write arithmetic

---
+++ README.md
@@ -0,0 +1,7 @@
+# References
+## Memory Initial Value
+- https://www.quora.com/A-memory-byte-is-never-empty-but-its-initial-content-may-be-meaningless-to-your-program-What-does-this-mean
+- https://softwareengineering.stackexchange.com/questions/53272/the-default-state-of-unused-memory
+## C-Instruction Label Translation
+- https://github.com/xctom/Nand2Tetris/blob/master/projects/07/src/CodeWriter.java
+- https://github.com/thomas-stockx/nand2tetris/blob/master/projects/07/VMTranslator/src/com/stockxit/nand2tetris/CodeWriter.java
--- src/main/java/CodeWriter.java
+++ src/main/java/CodeWriter.java
@@ -1,147 +1,109 @@
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.PrintWriter;
 
 public class CodeWriter {
-    private FileOutputStream fileOutputStream;
-    private int methodCallCounter = 1;
-
-    private final Map<String, String> commandToJump = new HashMap<>() {{
-        put(EQ, "JEQ");
-        put(GT, "JGT");
-        put(LT, "JLT");
-    }};
+    private PrintWriter printWriter;
+    private int labelCount = 0;
 
     public CodeWriter(FileOutputStream fileOutputStream) {
-        this.fileOutputStream = fileOutputStream;
+        this.printWriter = new PrintWriter(fileOutputStream);
+    }
+
+    private void useTwoStacksWithCInstruction(String cInstruction) {
+        printWriter.println("@SP");
+        printWriter.println("AM=M-1");
+        printWriter.println("D=M");
+        printWriter.println("A=A-1");
+        printWriter.println("M=D-M");
+        printWriter.println("@TRUE" + labelCount);
+        printWriter.println(cInstruction);
+        printWriter.println("@SP");
+        printWriter.println("A=M-1");
+        // https://www.quora.com/A-memory-byte-is-never-empty-but-its-initial-content-may-be-meaningless-to-your-program-What-does-this-mean
+        // https://softwareengineering.stackexchange.com/questions/53272/the-default-state-of-unused-memory
+        printWriter.println("M=-1");
+        printWriter.println("@END" + labelCount);
+        printWriter.println("0;JMP");
+        printWriter.println("(TRUE" + labelCount + ")");
+        printWriter.println("@SP");
+        printWriter.println("A=M-1");
+        printWriter.println("M=1");
+        printWriter.println("(END" + labelCount + ")");
     }
 
     public void writeArithmetic(String command) {
-        List<String> assemblyCommands = new ArrayList<>();
-        assemblyCommands.add("");
-
-        if (command.equals("add")) {
-            doAdd(assemblyCommands);
+        // C-Instruction 이 특별하다는 것을 인지해야 풀 수 있는 문제다. 다음과 같은 항목이 특이하다.
+        // 1. label 이 사용된다.
+        // 2. label 의 이름은 high level 변수 짓듯 할 수 있지 않고, index 를 부여해 구분해야 한다.
+        if (command.equals("eq")) {
+            printWriter.println("// eq");
+            useTwoStacksWithCInstruction("D;JEQ");
+            labelCount++;
+        } else if (command.equals("gt")) {
+            printWriter.println("// gt");
+            useTwoStacksWithCInstruction("D;JGT");
+            labelCount++;
+        } else if (command.equals("lt")) {
+            printWriter.println("// lt");
+            useTwoStacksWithCInstruction("D;JLT");
+            labelCount++;
+        } else if (command.equals("add")) {
+            printWriter.println("// add");
+            printWriter.println("@SP");
+            printWriter.println("AM=M-1");
+            printWriter.println("D=M");
+            printWriter.println("A=A-1");
+            printWriter.println("M=D+M");
         } else if (command.equals("sub")) {
-            doSub(assemblyCommands);
-        } else if (command.equals("neg")) {
-            doNeg(assemblyCommands);
+            printWriter.println("// sub");
+            printWriter.println("@SP");
+            printWriter.println("AM=M-1");
+            printWriter.println("D=M");
+            printWriter.println("A=A-1");
+            printWriter.println("M=M-D");
         } else if (command.equals("and")) {
-            doAnd(assemblyCommands);
+            printWriter.println("// and");
+            printWriter.println("@SP");
+            printWriter.println("AM=M-1");
+            printWriter.println("D=M");
+            printWriter.println("A=A-1");
+            printWriter.println("M=D&M");
         } else if (command.equals("or")) {
-            doOr(assemblyCommands);
+            printWriter.println("// or");
+            printWriter.println("@SP");
+            printWriter.println("AM=M-1");
+            printWriter.println("D=M");
+            printWriter.println("A=A-1");
+            printWriter.println("M=D|M");
+        } else if (command.equals("neg")) {
+            printWriter.println("// neg");
+            printWriter.println("@SP");
+            printWriter.println("A=M-1");
+            printWriter.println("M=-M");
         } else if (command.equals("not")) {
-            doNot(assemblyCommands);
-        } else if (command.equals(EQ) ||
-                command.equals(GT) ||
-                command.equals(LT)
-        ) {
-            if (command.equals(EQ)) {
-                assemblyCommands.add("// eq");
-            } else if (command.equals(GT)) {
-                assemblyCommands.add("// gt");
-            } else if (command.equals(LT)) {
-                assemblyCommands.add("// lt");
-            }
-            getPopToDRegister(assemblyCommands);
-            getPeekToARegister(assemblyCommands);
-            assemblyCommands.add("D=M-D");
-            assemblyCommands.add("@IF" + methodCallCounter);
-            assemblyCommands.add("D;" + commandToJump.get(command));
-
-            assemblyCommands.add("@END" + methodCallCounter);
-            assemblyCommands.add("M=0");
-            assemblyCommands.add("0;JMP");
-
-            assemblyCommands.add("(IF" + methodCallCounter + ")");
-            assemblyCommands.add("M=1");
-
-            assemblyCommands.add("(END" + methodCallCounter + ")");
-        }
-
-        methodCallCounter++;
-
-        String joinedCommand = String.join(System.lineSeparator(), assemblyCommands);
-        writeToOutputStream(joinedCommand);
-    }
-
-    private void doAdd(List<String> assemblyCommands) {
-        assemblyCommands.add("// add");
-        getPopToDRegister(assemblyCommands);
-        getPeekToARegister(assemblyCommands);
-        assemblyCommands.add("M=D+M");
-    }
-
-    private void getPopToDRegister(List<String> assemblyCommands) {
-        assemblyCommands.add(STACK_POINTER);
-        assemblyCommands.add("AM=M-1");
-        assemblyCommands.add("D=M");
-    }
-
-    private void getPeekToARegister(List<String> assemblyCommands) {
-        assemblyCommands.add(STACK_POINTER);
-        assemblyCommands.add("A=M-1");
-    }
-
-    private void doSub(List<String> assemblyCommands) {
-        assemblyCommands.add("// sub");
-        getPopToDRegister(assemblyCommands);
-        getPeekToARegister(assemblyCommands);
-        assemblyCommands.add("M=M-D");
-    }
-
-    private void doNeg(List<String> assemblyCommands) {
-        assemblyCommands.add("// neg");
-        getPeekToARegister(assemblyCommands);
-        assemblyCommands.add("M=-M");
-    }
-
-    private void doAnd(List<String> assemblyCommands) {
-        assemblyCommands.add("// and");
-        getPopToDRegister(assemblyCommands);
-        getPeekToARegister(assemblyCommands);
-        assemblyCommands.add("M=D&M");
-    }
-
-    private void doOr(List<String> assemblyCommands) {
-        assemblyCommands.add("// or");
-        getPopToDRegister(assemblyCommands);
-        getPeekToARegister(assemblyCommands);
-        assemblyCommands.add("M=D|M");
-    }
-
-    private void doNot(List<String> assemblyCommands) {
-        assemblyCommands.add("// not");
-        getPeekToARegister(assemblyCommands);
-        assemblyCommands.add("M=!M");
-    }
-
-    private final String STACK_POINTER = "@SP";
-
-    private final String EQ = "eq";
-    private final String LT = "lt";
-    private final String GT = "gt";
-
-    private void writeToOutputStream(String data) {
-        try {
-            fileOutputStream.write(data.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
+            printWriter.println("// not");
+            printWriter.println("@SP");
+            printWriter.println("A=M-1");
+            printWriter.println("M=!M");
         }
     }
 
-    public void writePushPop(String command, String segment, int index) {
+    public void writePushPop(int command, String segment, int index) {
+        // TODO("Not yet implemented.")
+        if (command == Parser.C_PUSH) {
 
+        }
+        // TODO("Not yet implemented.")
+        else if (command == Parser.C_POP) {
+
+        } else {
+            throw new RuntimeException("Command" + command + "is not push or pop.");
+        }
     }
 
     public void close() {
-        try {
-            fileOutputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (printWriter != null) {
+            printWriter.close();
         }
     }
 }
