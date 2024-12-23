7f5b648fee75ecfcd03ec25b4aeb1d0e441b8b78
Scoobi-wisdoom <grevod@gmail.com>
2024-12-24 02:51:35 +0900

    add method: get keyword

--- src/main/java/Keyword.java
+++ src/main/java/Keyword.java
@@ -45,4 +45,31 @@ public enum Keyword {
             "null",
             "this",
     };
+
+    public static Keyword getKeyword(String token) {
+        return switch (token) {
+            case "class" -> Keyword.CLASS;
+            case "method" -> Keyword.METHOD;
+            case "function" -> Keyword.FUNCTION;
+            case "constructor" -> Keyword.CONSTRUCTOR;
+            case "int" -> Keyword.INT;
+            case "boolean" -> Keyword.BOOLEAN;
+            case "char" -> Keyword.CHAR;
+            case "void" -> Keyword.VOID;
+            case "var" -> Keyword.VAR;
+            case "static" -> Keyword.STATIC;
+            case "field" -> Keyword.FIELD;
+            case "let" -> Keyword.LET;
+            case "do" -> Keyword.DO;
+            case "if" -> Keyword.IF;
+            case "else" -> Keyword.ELSE;
+            case "while" -> Keyword.WHILE;
+            case "return" -> Keyword.RETURN;
+            case "true" -> Keyword.TRUE;
+            case "false" -> Keyword.FALSE;
+            case "null" -> Keyword.NULL;
+            case "this" -> Keyword.THIS;
+            default -> throw new RuntimeException(token + " is not a keyword.");
+        };
+    }
 }
